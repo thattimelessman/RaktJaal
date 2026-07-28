c1821c65f4cfca9ed9a7eac60f9e6c338a75c8d9
thattimelessman <agrajsingh04@gmail.com>
2026-07-28 10:19:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RaktJaal from "./RaktJaal.jsx";
 import AuthPage from "./Authpage.jsx";
 import ProfilePage from "./Profilepage.jsx";
-import ActionPage from "./Actionpage.jsx";
+import ActionPage from "./ActionPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
